5263df67624063e102fb1d4400ecbefee87a27da
tonnymuchui <tonykanyingah@gmail.com>
2023-01-11 13:56:50 +0300

    update looping ticketlist

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,19 +1,33 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const mainTicketList = [
+    {
+      names: 'Thato and Haley',
+      location: '3A',
+      issue: 'Firebase won\'t save record. Halp.'
+    },
+    {
+      names: 'Sleater and Kinney',
+      location: '4B',
+      issue: 'Prop types are throwing an error.'
+    },
+    {
+      names: 'Imani & Jacob',
+      location: '9F',
+      issue: 'Child component isn\'t rendering.'
+    }
+  ];
+
 function TicketList() {
   return (
     <React.Fragment>
-      <Ticket
-        location="3AA"
-        names="Thato and Haley"
-        issue="Firebase will not save record!"
-      />
-      <Ticket
-        location="4B"
-        names="Sleater and Kinney"
-        issue="Prop types are throwing an error."
-      />
+        {mainTicketList.map((ticket, index) => 
+        <Ticket names={ticket.names}
+        location={index}
+        issue={ticket.issue}
+        key={index} />
+        )}
     </React.Fragment>
   );
 }
