b8aeab46f3d9e2866a54afa4a12e3bed5a9cd19f
tonnymuchui <tonykanyingah@gmail.com>
2023-01-11 13:06:56 +0300

    refuctor app component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+import TicketList from './TicketList';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header />
+    <TicketList />
+    </React.Fragment>
+  );
+}
+
+export default App;
