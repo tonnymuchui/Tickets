28423556083b93b77756c235f30f5d497fe51774
tonnymuchui <tonykanyingah@gmail.com>
2023-01-12 23:14:42 +0300

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../App.css';
 import Header from './Header';
-import TicketList from './TicketList';
+import TicketControl from './TicketControl';
 
 function App() {
   return (
     <React.Fragment>
     <Header />
-    <TicketList />
+    <TicketControl />
     </React.Fragment>
   );
 }
