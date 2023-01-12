962660637cf292deb92b278e64049db026cfab35
tonnymuchui <tonykanyingah@gmail.com>
2023-01-12 10:32:33 +0300

    Add ticketform

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NewTicketForm(props) {
+    return(
+        <React.Fragment>
+            <h3>This is a form.</h3>
+        </React.Fragment>
+    );
+}
+export default NewTicketForm;
