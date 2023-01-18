f23029d0d7fa4ded10e9be482c76bb762a41c81c
tonnymuchui <tonykanyingah@gmail.com>
2023-01-18 23:24:11 +0300

    Add selected ticket state

---
+++ src/components/TicketDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TicketDetail(props){
+  return (
+    <React.Fragment>
+      <h1>Ticket Detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default TicketDetail;
