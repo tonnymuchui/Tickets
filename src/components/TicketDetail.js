9c2dc5744f529e0384b050e96c268abf3c4d0449
tonnymuchui <tonykanyingah@gmail.com>
2023-01-24 23:13:39 +0300

    Update ticketdetails

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TicketDetail(props){
+  const { ticket, onClickingDelete } = props;
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
+      <button onClick={()=> onClickingDelete(ticket.id) }>Close Ticket</button>
       <hr/>
     </React.Fragment>
   );
 }
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func
+};
 
 export default TicketDetail;
