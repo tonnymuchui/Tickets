9f2bae9613bbc16db3c0004a8f82b230df8e2c99
tonnymuchui <tonykanyingah@gmail.com>
2023-01-24 23:13:56 +0300

    Update ticketlist

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -5,17 +5,21 @@ import PropTypes from "prop-types";
 function TicketList(props) {
   return (
     <React.Fragment>
-        {props.ticketList.map((ticket, index) => 
-        <Ticket names={ticket.names}
-        location={index}
-        issue={ticket.issue}
-        key={index} />
+        {props.ticketList.map((ticket) => 
+        <Ticket
+        whenTicketClicked = {props.onTicketSelected}
+        names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          id={ticket.id}
+          key={ticket.id}/>
         )}
     </React.Fragment>
   );
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
 }
 export default TicketList;
