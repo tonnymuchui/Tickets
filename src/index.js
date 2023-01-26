98f30c9826d984c7287c1227f200f4d415436174
tonnymuchui <tonykanyingah@gmail.com>
2023-01-26 22:28:46 +0300

    Add redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
 
+
+const store = createStore(reducer);
+store.subscribe(() =>
+  console.log(store.getState())
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
